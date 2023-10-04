7973664db065e451a84fd06bd15a6353ace2f149
Srijon Biswas <60281395+DSrijon01@users.noreply.github.com>
2023-10-04 14:38:21 +0600

    Demo App Execution Fucntional

---
+++ Android Demo/LanuchApp.py
@@ -0,0 +1,16 @@
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['automationName'] = 'UiAutomator2'
+desired_caps['platformVersion'] = '11'
+desired_caps['deviceName'] = 'Pixel3XL'
+desired_caps['app'] = ('/Users/sujithreddy/Documents/Code2Lead/Others/kwad.apk')
+desired_caps['appPackage'] = 'com.code2lead.kwad'
+desired_caps['appActivity'] = 'com.code2lead.kwad.MainActivity'
+
+driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
+
+ele_id = driver.find_element(AppiumBy.ID, "com.code2lead.kwad:id/EnterValue")
+ele_id.click()
--- Tc_01_Demo.py
+++ Tc_01_Demo.py
@@ -1,5 +1,7 @@
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.support.wait import WebDriverWait
 
 desired_cap = {
   "deviceName": "Android Emulator",
@@ -12,4 +14,9 @@ desired_cap = {
 
 options = UiAutomator2Options().load_capabilities(desired_cap)
 driver = webdriver.Remote("http://localhost:4723/wd/hub", options=options)
+
+## Click on the Enter Some Value button
+element_id = driver.find_element(AppiumBy.ID, "com.code2lead.kwad:id/EnterValue")
+wait = WebDriverWait(driver, 20)
+element_id.click()
 
