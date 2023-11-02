b9e975db068a9f5f878fadc7c32741dee00f7166
Srijon Biswas <60281395+DSrijon01@users.noreply.github.com>
2023-11-02 22:41:15 +0600

    Login Page POM Initiated to the Framework

---
+++ AppiumFrameWork/tests/LoginTest.py
@@ -0,0 +1,38 @@
+import unittest
+import pytest
+import AppiumFrameWork.utilities.logger as cl
+from AppiumFrameWork.base.BasePage import BasePage
+from AppiumFrameWork.pages.LoginPage import LoginPage
+
+
+@pytest.mark.usefixtures("beforeClass", "beforeMethod")
+class LoginTest(unittest.TestCase):
+
+    @pytest.fixture(autouse=True)
+    def classObjects(self):
+        self.gt = LoginPage(self.driver)
+        self.bp = BasePage(self.driver)
+
+    @pytest.mark.run(order=5)
+    def test_enterDataInEditBox(self):
+        self.gt.enterText()
+        self.gt.clickOnSubmit()
+
+    @pytest.mark.run(order=4)
+    def test_openLoginScreen(self):
+        self.bp.keyCode(4)
+        self.gt.clickLoginButton()
+        self.gt.enterEmail()
+        self.gt.enterPassword()
+        self.gt.clickLoginButton2()
+        self.gt.verifyAdminScreen()
+
+    ## Failed Scenario Identification
+    @pytest.mark.run(order=3)
+    def test_loginFailMethod(self):
+        cl.allureLogs("App Opened")
+        self.gt.clickLoginButton()
+        self.gt.enterEmail()
+        self.gt.enterPassword2()
+        self.gt.clickLoginButton2()
+        self.gt.verifyAdminScreen()
